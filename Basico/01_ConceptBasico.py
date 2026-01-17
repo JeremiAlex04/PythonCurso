69ad06e639b272c45c3481acfed7e26462658bf3
jeremialex04 <jeremialex04@gmail.com>
2026-01-17 12:07:57 -0500

    Conceptos Basicos

---
+++ Basico/01_ConceptBasico.py
@@ -0,0 +1,77 @@
+## Para Iniciar
+## Print - Mostrar informacion en consola
+
+## TEXTO
+
+## Con comillas
+print("Hola Mundo")
+print("Hola Señores")
+
+## Con comillas simples
+print('Hola Señores de Github')
+
+## Se paracion por elementos
+print("Hola","Dev","de","Github")
+
+## Separadores
+print("Carpeta","de","juegos", sep="-")
+
+## Sin salto de linea
+print("Se imprime", end="!")
+print("Luego esto")
+
+# Mostrar un cadena multilinea
+
+'''
+Hola estudiantes de pyton este es el 
+nuevo tema del dia
+'''
+
+
+# NUMEROS
+
+# Imprimir en numeros
+print(2)
+
+# Imporesion de numeros enteros con operacion
+print(2+3)
+
+# Revisar que tipo de dato es 
+print(type("Hola"))
+print(type(10))
+print(type(-10))
+
+# Se logran mostrar datos extensos
+print(23144343243523432132)
+
+# Nro - INT
+print(4)
+
+# Nro - Float
+print(4.3)
+
+# Nro - Boleanos
+print(type(True))
+print(type(False))
+
+# Nro - complejos
+print(1+2j)
+
+# NoneType - Ausencia de valor
+print(None)
+
+
+# Tranformar el tipo de valor - Casting de tipos
+# Transformar un tipo de valor a otro
+
+print("Conversion de Tipos")
+
+print(int("100")) # 100
+print(float("3.1456")) # 3
+print(int(2.345)) ## A qui no redondea solo ignora el decimal y se queda con el valor entero
+print("10"+str(12)) # 1012
+
+print(bool("")) # false
+print(bool(" ")) # true
+print(bool("False")) ## true: porque tiene contenido
+
